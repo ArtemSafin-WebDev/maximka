8d4449aef7ac6f137f228dbdf3b98c6f5ff5d123
Artem Safin <artemsafin.webdev@gmail.com>
2026-05-28 10:54:40 +0300

    Fix

--- src/js/classes/components/ProgressSection.ts
+++ src/js/classes/components/ProgressSection.ts
@@ -1,5 +1,5 @@
 import Swiper from "swiper";
-import { Scrollbar } from "swiper/modules";
+import { Navigation, Scrollbar } from "swiper/modules";
 
 import { MOBILE_BREAKPOINT } from "../../constants/breakpoints";
 import Component from "../Component";
@@ -9,59 +9,8 @@ class ProgressSection extends Component {
   private readonly slides: HTMLElement[];
   private readonly filterInputs: HTMLInputElement[];
   private readonly emptyElement: HTMLElement | null;
-  private readonly prevButton: HTMLButtonElement | null;
-  private readonly nextButton: HTMLButtonElement | null;
   private readonly swiper: Swiper | null;
-  private readonly desktopMediaQuery = window.matchMedia(
-    `(min-width: ${MOBILE_BREAKPOINT + 1}px)`
-  );
-  private readonly handleMediaChange = () => this.updateNavigationState();
-  private readonly handlePrevClick = () => {
-    if (
-      this.desktopMediaQuery.matches &&
-      this.swiper?.isBeginning &&
-      !this.swiper.isLocked
-    ) {
-      this.slideToLastVisible();
-      return;
-    }
-
-    this.swiper?.slidePrev();
-  };
-  private readonly handleNextClick = () => {
-    if (
-      this.desktopMediaQuery.matches &&
-      this.swiper?.isEnd &&
-      !this.swiper.isLocked
-    ) {
-      this.swiper.slideTo(0);
-      return;
-    }
-
-    this.swiper?.slideNext();
-  };
   private readonly handleFilterChange = () => this.updateFilter();
-  private readonly updateNavigationState = () => {
-    const isDesktop = this.desktopMediaQuery.matches;
-    const isLocked = Boolean(this.swiper?.isLocked);
-
-    this.prevButton?.classList.toggle(
-      "swiper-button-disabled",
-      isLocked || (!isDesktop && Boolean(this.swiper?.isBeginning))
-    );
-    this.prevButton?.toggleAttribute(
-      "disabled",
-      isLocked || (!isDesktop && Boolean(this.swiper?.isBeginning))
-    );
-    this.nextButton?.classList.toggle(
-      "swiper-button-disabled",
-      isLocked || (!isDesktop && Boolean(this.swiper?.isEnd))
-    );
-    this.nextButton?.toggleAttribute(
-      "disabled",
-      isLocked || (!isDesktop && Boolean(this.swiper?.isEnd))
-    );
-  };
 
   constructor(element: HTMLElement) {
     super(element);
@@ -75,41 +24,37 @@ class ProgressSection extends Component {
     );
     this.emptyElement =
       this.element.querySelector<HTMLElement>(".js-progress-empty");
-    this.prevButton =
-      this.element.querySelector<HTMLButtonElement>(".js-progress-prev");
-    this.nextButton =
-      this.element.querySelector<HTMLButtonElement>(".js-progress-next");
     this.swiper = this.slider ? this.initSlider(this.slider) : null;
 
-    this.prevButton?.addEventListener("click", this.handlePrevClick);
-    this.nextButton?.addEventListener("click", this.handleNextClick);
-    this.desktopMediaQuery.addEventListener("change", this.handleMediaChange);
-    this.swiper?.on("slideChange", this.updateNavigationState);
-    this.swiper?.on("update", this.updateNavigationState);
     this.filterInputs.forEach((input) => {
       input.addEventListener("change", this.handleFilterChange);
     });
 
     this.updateFilter();
-    this.updateNavigationState();
   }
 
   private initSlider(slider: HTMLElement) {
     return new Swiper(slider, {
-      modules: [Scrollbar],
+      modules: [Navigation, Scrollbar],
       slidesPerView: "auto",
       spaceBetween: 20,
       speed: 500,
       watchOverflow: true,
+      navigation: {
+        prevEl: slider.querySelector<HTMLButtonElement>(".js-progress-prev"),
+        nextEl: slider.querySelector<HTMLButtonElement>(".js-progress-next"),
+      },
       scrollbar: {
         el: slider.querySelector<HTMLElement>(".swiper-scrollbar"),
         draggable: true,
       },
       breakpoints: {
         0: {
+          loop: false,
           spaceBetween: 12,
         },
-        577: {
+        [MOBILE_BREAKPOINT + 1]: {
+          loop: true,
           spaceBetween: 20,
         },
       },
@@ -138,7 +83,6 @@ class ProgressSection extends Component {
 
     this.swiper?.slideTo(0, 0);
     this.swiper?.update();
-    this.updateNavigationState();
   }
 
   private getCheckedValue(name: string) {
@@ -149,21 +93,7 @@ class ProgressSection extends Component {
     return checkedInput?.value ?? "";
   }
 
-  private slideToLastVisible() {
-    const lastVisibleSlideIndex = this.slides.reduce(
-      (lastIndex, slide, index) => (slide.hidden ? lastIndex : index),
-      0
-    );
-
-    this.swiper?.slideTo(lastVisibleSlideIndex);
-  }
-
   public destroy() {
-    this.prevButton?.removeEventListener("click", this.handlePrevClick);
-    this.nextButton?.removeEventListener("click", this.handleNextClick);
-    this.desktopMediaQuery.removeEventListener("change", this.handleMediaChange);
-    this.swiper?.off("slideChange", this.updateNavigationState);
-    this.swiper?.off("update", this.updateNavigationState);
     this.filterInputs.forEach((input) => {
       input.removeEventListener("change", this.handleFilterChange);
     });
