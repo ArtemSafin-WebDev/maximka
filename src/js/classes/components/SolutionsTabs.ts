2f88d7949338d0621c13c1fa852ebb38c1f6096f
Artem Safin <artemsafin.webdev@gmail.com>
2026-06-01 00:06:54 +0300

    По адаптивам

--- src/js/classes/components/SolutionsTabs.ts
+++ src/js/classes/components/SolutionsTabs.ts
@@ -1,12 +1,8 @@
-import Swiper from "swiper";
-import { Scrollbar } from "swiper/modules";
-
 import Component from "../Component";
 
 class SolutionsTabs extends Component {
   private readonly tabs: HTMLButtonElement[];
   private readonly panels: HTMLElement[];
-  private readonly swipers: Swiper[];
   private readonly handleTabClick = (event: Event) => {
     const tab = event.currentTarget as HTMLButtonElement;
     this.setActive(this.tabs.indexOf(tab));
@@ -22,40 +18,12 @@ class SolutionsTabs extends Component {
     this.panels = Array.from(
       this.element.querySelectorAll<HTMLElement>(".js-solutions-panel")
     );
-    this.swipers = this.panels
-      .map((panel) => panel.querySelector<HTMLElement>(".js-solutions-slider"))
-      .filter((slider): slider is HTMLElement => Boolean(slider))
-      .map((slider) => this.initSlider(slider));
 
     this.tabs.forEach((tab) => {
       tab.addEventListener("click", this.handleTabClick);
     });
 
     this.setActive(0);
-  }
-
-  private initSlider(slider: HTMLElement) {
-    return new Swiper(slider, {
-      modules: [Scrollbar],
-      slidesPerView: "auto",
-      spaceBetween: 20,
-      speed: 500,
-      watchOverflow: true,
-      scrollbar: {
-        el: slider.querySelector<HTMLElement>(".swiper-scrollbar"),
-        draggable: true,
-      },
-      breakpoints: {
-        0: {
-          enabled: false,
-          spaceBetween: 11,
-        },
-        577: {
-          enabled: true,
-          spaceBetween: 20,
-        },
-      },
-    });
   }
 
   private setActive(index: number) {
@@ -77,14 +45,12 @@ class SolutionsTabs extends Component {
     });
 
     this.activeIndex = index;
-    this.swipers[index]?.update();
   }
 
   public destroy() {
     this.tabs.forEach((tab) => {
       tab.removeEventListener("click", this.handleTabClick);
     });
-    this.swipers.forEach((swiper) => swiper.destroy(true, true));
     this.unregister();
   }
 }
